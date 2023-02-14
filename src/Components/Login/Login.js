1ec24b8ee58beef8d1bbeb8dbd7677297c11cd8a
Tubsen <tobianwennink12@gmail.com>
2023-02-14 11:19:21 +0100

    new react app

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+const Login = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+        </div>
+    );
+}
+
+export default Login;
