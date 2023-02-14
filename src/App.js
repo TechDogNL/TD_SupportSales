bcd399c283e68e45d907c3665183814f2c6782c5
Tubsen <tobianwennink12@gmail.com>
2023-02-14 11:26:47 +0100

    Not Found page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Components/Login/Login";
+import NotFound from "./Components/NotFound/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />}></Route>
+        <Route path="/*" element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
   );
