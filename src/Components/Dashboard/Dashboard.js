3e8c93c9e89e89c20cb2def52a598ee4e9636f3d
Tubsen <tobianwennink12@gmail.com>
2023-02-15 08:34:13 +0100

    push

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import './Dashboard.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 const Dashboard = () => {
     return (
-        <div className='container-fluid bg-light vh-100 '>
-            <div>
-                
+        <div>
+            <Navbar />
+            <div className='body container-fluid vh-100'>
+                hoi
             </div>
         </div>
     );
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import logo from './logotechdoggroupmetpayoff.png';
+import './Navbar.css';
+import { Button, Popover, PopoverBody } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const Navbar = () => {
+
+const [selectedDate, setSelectedDate] = useState(null);
+
+const [popoverOpen, setPopoverOpen] = useState(false);
+
     return (
         <div>
+            <nav className="navbar navbar-light bg-light">
+                <div className="container-fluid d-flex justify-content-center ">
+                    <div className='col-3'>
+
+                    </div>
+                    <div className='col-6 d-flex justify-content-center'>
+                        <img src={logo} id='logo' className="img-fluid" alt=""></img>
+                    </div>
+                    <div className='col-3'>
+                    <Button
+              type="button"
+              color="secondary"
+              id="popoverButton"
+              data-bs-toggle="popover"
+              title="Popup title"
+              data-bs-content="Popup content"
+              onClick={e => setPopoverOpen(!popoverOpen)}
+            >
+              Selecteer datum
+            </Button>
+            <Popover
+              placement="bottom"
+              isOpen={popoverOpen}
+              target="popoverButton"
+              toggle={e => setPopoverOpen(!popoverOpen)}
+            >
+              <PopoverBody> 
+              </PopoverBody>
+            </Popover>
+                    </div>
+                </div>
+            </nav>
         </div>
     );
 }
